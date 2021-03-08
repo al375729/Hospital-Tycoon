90942ef2382207d7a0edbc291c70a1cecd3981b5
al375729 <57604778+al375729@users.noreply.github.com>
2021-03-08 23:56:58 +0100

    Generador de trabajadores aleatorios y populate de la UI

---
+++ Assets/Scripts/Workers/WorkerScriptableObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WorkerObjects", menuName = "ScriptableObjects/WorkerObjects")]
+public class WorkerScriptableObject : ScriptableObject
+{
+    public string name;
+    public Sprite image;
+    public string description;
+    public GameObject prefab;
+}
+
+
+
